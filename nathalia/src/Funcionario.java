797b3fff03cea7251407b05632dea9235f6eb9e3
Nathalia <natizinhabarcelos@gmail.com>
2017-07-21 11:06:49 -0300

    deiosnajthok

---
+++ nathalia/src/Funcionario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Funcionario {
+    
+    public class funcionário extends Pessoa {
+  
+    int codfuncionario;
+    String cartTrabalho;
+    String tipodesangue;
+    String planodesaude;
+    float salario;
+    String banco;
+    String conta;
+    }
+    
+}
